a220e323c10fb5377ba2b572f3a4c6d09523c71d
Yu Brian She <YB@Yus-MacBook-Air.local>
2016-09-25 16:01:48 +0200

    small fix

--- Assignment 2/IncSgmScan/ScanHost.cu.h
+++ Assignment 2/IncSgmScan/ScanHost.cu.h
@@ -4,7 +4,7 @@
 #include "ScanKernels.cu.h"
 
 #include <sys/time.h>
-#include <time.h> 
+#include <time.h>
 
 int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval *t1)
 {
@@ -16,24 +16,24 @@ int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval 
 }
 
 /**
- * block_size is the size of the cuda block (must be a multiple 
+ * block_size is the size of the cuda block (must be a multiple
  *                of 32 less than 1025)
  * d_size     is the size of both the input and output arrays.
  * d_in       is the device array; it is supposably
  *                allocated and holds valid values (input).
- * d_out      is the output GPU array -- if you want 
+ * d_out      is the output GPU array -- if you want
  *            its data on CPU needs to copy it back to host.
  *
- * OP         class denotes the associative binary operator 
- *                and should have an implementation similar to 
+ * OP         class denotes the associative binary operator
+ *                and should have an implementation similar to
  *                `class Add' in ScanUtil.cu, i.e., exporting
  *                `identity' and `apply' functions.
- * T          denotes the type on which OP operates, 
- *                e.g., float or int. 
+ * T          denotes the type on which OP operates,
+ *                e.g., float or int.
  */
 template<class OP, class T>
 void scanInc(    unsigned int  block_size,
-                 unsigned long d_size, 
+                 unsigned long d_size,
                  T*            d_in,  // device
                  T*            d_out  // device
 ) {
@@ -46,7 +46,7 @@ void scanInc(    unsigned int  block_size,
 
     scanIncKernel<OP,T><<< num_blocks, block_size, sh_mem_size >>>(d_in, d_out, d_size);
     cudaThreadSynchronize();
-    
+
     if (block_size >= d_size) { return; }
 
     /**********************/
@@ -60,16 +60,16 @@ void scanInc(    unsigned int  block_size,
 
     unsigned int num_blocks_rec = ( (num_blocks % block_size) == 0 ) ?
                                   num_blocks / block_size     :
-                                  num_blocks / block_size + 1 ; 
+                                  num_blocks / block_size + 1 ;
 
-    //   2. copy in the end-of-block results of the previous scan 
+    //   2. copy in the end-of-block results of the previous scan
     copyEndOfBlockKernel<T><<< num_blocks_rec, block_size >>>(d_out, d_rec_in, num_blocks);
     cudaThreadSynchronize();
 
     //   3. scan recursively the last elements of each CUDA block
     scanInc<OP,T>( block_size, num_blocks, d_rec_in, d_rec_out );
 
-    //   4. distribute the the corresponding element of the 
+    //   4. distribute the the corresponding element of the
     //      recursively scanned data to all elements of the
     //      corresponding original block
     distributeEndBlock<OP,T><<< num_blocks, block_size >>>(d_rec_out, d_out, d_size);
@@ -82,22 +82,22 @@ void scanInc(    unsigned int  block_size,
 
 
 /**
- * block_size is the size of the cuda block (must be a multiple 
+ * block_size is the size of the cuda block (must be a multiple
  *                of 32 less than 1025)
  * d_size     is the size of both the input and output arrays.
  * d_in       is the device array; it is supposably
  *                allocated and holds valid values (input).
- * flags      is the flag array, in which !=0 indicates 
+ * flags      is the flag array, in which !=0 indicates
  *                start of a segment.
- * d_out      is the output GPU array -- if you want 
+ * d_out      is the output GPU array -- if you want
  *            its data on CPU you need to copy it back to host.
  *
- * OP         class denotes the associative binary operator 
- *                and should have an implementation similar to 
+ * OP         class denotes the associative binary operator
+ *                and should have an implementation similar to
  *                `class Add' in ScanUtil.cu, i.e., exporting
  *                `identity' and `apply' functions.
- * T          denotes the type on which OP operates, 
- *                e.g., float or int. 
+ * T          denotes the type on which OP operates,
+ *                e.g., float or int.
  */
 template<class OP, class T>
 void sgmScanInc( const unsigned int  block_size,
@@ -143,12 +143,12 @@ void sgmScanInc( const unsigned int  block_size,
     //      and different than zero otherwise. This is implemented
     //      as a CUDA-block level inclusive scan on the flag array,
     //      i.e., the segment that start the block has zero-flags,
-    //      which will be preserved by the inclusive scan. 
+    //      which will be preserved by the inclusive scan.
     scanIncKernel<Add<int>,int> <<< num_blocks, block_size, flg_sh_size >>>
                 ( flags, f_inds, d_size );
 
     //   4. finally, accumulate the recursive result of segmented scan
-    //      to the elements from the first segment of each block (if 
+    //      to the elements from the first segment of each block (if
     //      segment is open).
     sgmDistributeEndBlock <OP,T> <<< num_blocks, block_size >>>
                 ( d_rec_out, d_out, f_inds, d_size );
@@ -166,18 +166,18 @@ void scanExcl(const unsigned int block_size,
               T* d_in,
               T* d_out){
     unsigned int num_blocks;
-    
+
     num_blocks = ( (d_size % block_size) == 0) ?
     d_size / block_size     :
     d_size / block_size + 1 ;
-    
+
     T* scanInc_res;
     cudaMalloc((void**) &scanInc_res, d_size*sizeof(T));
     scanInc<OP,T>(block_size, d_size, d_in, scanInc_res);
     //shift it
     shiftRightByOne<T><<<num_blocks, block_size>>>(scanInc_res, d_out, OP::identity(), d_size);
     cudaThreadSynchronize();
-    cudeFree(scanInc_res);
+    cudaFree(scanInc_res);
 }
 
 template<class OP, class T>
@@ -187,17 +187,17 @@ void sgmScanExcl(const unsigned int block_size,
               int* flags,
               T* d_out){
     unsigned int num_blocks;
-    
+
     num_blocks = ( (d_size % block_size) == 0) ?
     d_size / block_size     :
     d_size / block_size + 1 ;
-    
+
     T* sgmScanInc_res;
     cudaMalloc((void**) &sgmScanInc_res, d_size*sizeof(T));
     sgmScanInc<OP,T>(block_size, d_size, d_in, flags, sgmScanInc_res);
     //shift it
-    sgmShiftRightByOne<T><<<num_blocks, block_size>>>(scanInc_res, flags, d_out, OP::identity(), d_size);
+    sgmShiftRightByOne<T><<<num_blocks, block_size>>>(sgmscanInc_res, flags, d_out, OP::identity(), d_size);
     cudaThreadSynchronize();
-    cudeFree(scanInc_res);
+    cudaFree(scanInc_res);
 }
 #endif //SCAN_HOST
