eeafc7649fb1627a9bc5da5eaf32106ffa5a30dd
Yu Brian She <YB@Yus-MacBook-Air.local>
2016-10-03 17:46:24 +0200

    fixes

--- Assignment 3/main.cu
+++ Assignment 3/main.cu
@@ -32,6 +32,6 @@ int main() {
 	printf("Sequential transpose test: %lu microseconds.\n", elapsed);
 
 	printf("Sequential transpose test:\n");
-	valid_trans<float>(h_A,h_B,ROWS,COLS);
+	compare<float>(h_A,h_B,ROWS,COLS);
 	return 0;
 }
