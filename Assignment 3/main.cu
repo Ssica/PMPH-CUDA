fdded23e25b3a650d46baac82919be4bd40ca89a
Yu Brian She <YB@Yus-MacBook-Air.local>
2016-11-05 22:57:04 +0100

    assignment 3

--- Assignment 3/main.cu
+++ Assignment 3/main.cu
@@ -190,7 +190,7 @@ int main() {
   cudaMemcpy(res_,d_res,res_mem,cudaMemcpyDeviceToHost);
 	printf("Task 3d: %lu microseconds.\n", elapsed);
   val = validate(m3_,res_,row1,col2, 0.01);
-	printf("Task3c matrix mult test: %d", val);
+	printf("Task3c matrix mult test: %d \n", val);
 	cudaFree(m1);
 	cudaFree(m2);
 	cudaFree(m3);
