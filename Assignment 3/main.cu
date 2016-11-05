6da36b423784bbf4122b398ede80ce9e85229c28
Yu Brian She <YB@Yus-MacBook-Air.local>
2016-11-05 22:53:14 +0100

    assignment 3

--- Assignment 3/main.cu
+++ Assignment 3/main.cu
@@ -181,14 +181,14 @@ int main() {
 
 	gettimeofday(&t_start,NULL);
 
-	task3c<float><<<grid,block>>>(d_m1,d_m2,d_res,row1,col1,col2);
+//	task3c<float><<<grid,block>>>(d_m1,d_m2,d_res,row1,col1,col2);
 
 	gettimeofday(&t_end,NULL);
 	timeval_subtract(&t_diff,&t_end,&t_start);
 	elapsed = (t_diff.tv_sec*1e6+t_diff.tv_usec);
 
-  cudaMemcpy(res_,d_res,res_mem,cudaMemcpyDeviceToHost);
-  val = validate(m1_,m3_,row1,col2, 0.01);
+  //cudaMemcpy(res_,d_res,res_mem,cudaMemcpyDeviceToHost);
+  //val = validate(m1_,m3_,row1,col2, 0.01);
 	printf("Task3c matrix mult test: %d", val);
 	cudaFree(m1);
 	cudaFree(m2);
