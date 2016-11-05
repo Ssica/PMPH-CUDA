0546ca26b1bd449d9ec82a3e99054f0d8ffc64ae
Yu Brian She <YB@Yus-MacBook-Air.local>
2016-11-05 22:52:24 +0100

    assignment 3

--- Assignment 3/task3c.cu.h
+++ Assignment 3/task3c.cu.h
@@ -8,7 +8,7 @@ __global__ void task3c(float* m1, float*m2, float* m_out, int row1, int col1, in
         for(int k = 0; k < col1; k++) {
             res = res + m1[j*col1+k] * m2[k*col2+i];
             }
-        res[j*col2+i] = res;
+        m_out[j*col2+i] = res;
         }
     else {
         return;
