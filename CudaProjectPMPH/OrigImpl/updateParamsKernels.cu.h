a1a5ef12e0d56a40bc45b80c2a3303f11e91464f
Yu Brian She <YB@Yus-MacBook-Air.local>
2016-11-05 16:17:17 +0100

    project changes

--- CudaProjectPMPH/OrigImpl/updateParamsKernels.cu.h
+++ CudaProjectPMPH/OrigImpl/updateParamsKernels.cu.h
@@ -33,13 +33,13 @@ __global__ void updateParamsKer(const unsigned g, const REAL alpha, const REAL b
 }
 
 
-__global__ void setParamsKer(REAL numX ,REAL numY, REAL* myX, REAL* myResult)
+__global__ void setParamsKer(REAL numX, REAL numY, REAL* myX, REAL* myResult)
 {
     unsigned int i = blockIdx.x*blockDim.x+threadIdx.x;
     unsigned int j = blockIdx.y*blockDim.y+threadIdx.y;
     unsigned int h = blockIdx.z;
     
-    myResult[h * numX * numY + i * numY + j] = 10; //(REAL) max(myX[i]-0.001*h, (REAL)0.0);
+    myResult[0] = (REAL) max(myX[i]-0.001*h, (REAL)0.0);
 
 }
 
